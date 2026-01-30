842c5d26c8a5bafbcfa30c2df82992c33ff1a046
ka8761 <dikachinwankwo575@gmail.com>
2026-01-30 18:49:46 +0100

    adjusted notfound in app.tsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login/Login.tsx';
 import Dashboard from './pages/Dashboard/Dashboard.tsx';
 import Users from './pages/Users/Users';
 import UserDetails from './pages/UserDetails/UserDetails';
-import NotFound from './pages/NotFound/NotFound';
+import NotFound from './pages/NotFound/NotFound.tsx';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout/Layout';
 import './index.css';
@@ -14,18 +14,21 @@ function App() {
     <SearchProvider>
       <Router>
         <Routes>
+      
           <Route path="/login" element={<Login />} />
 
+        
           <Route element={<ProtectedRoute />}>
             <Route element={<Layout />}>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/users/:id" element={<UserDetails />} />
+              <Route index element={<Dashboard />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="users" element={<Users />} />
+              <Route path="users/:id" element={<UserDetails />} />
+
+            
+              <Route path="*" element={<NotFound />} />
             </Route>
           </Route>
-
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </SearchProvider>
